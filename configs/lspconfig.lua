a7c532bce1775afefa687c2f731b705e6433ce40
Akumanory <thesteelgrace@gmail.com>
2024-03-19 01:13:18 +0300

    [+] add cmd params for clangd start cofiguration

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -5,8 +5,18 @@ local capabilities = base.capabilities
 local lspconfig = require('lspconfig')
 
 lspconfig.clangd.setup {
-  cmd = {"clangd",
-         "--completion-style=detailed"},
+  cmd = {   "clangd",
+            "--completion-style=detailed",
+            "--background-index",
+            "-j=8",
+            "--clang-tidy",
+            -- "--clang-tidy-checks=*",
+            "--all-scopes-completion",
+            "--cross-file-rename",
+            "--header-insertion-decorators",
+            "--header-insertion=iwyu",
+            "--pch-storage=memory"
+        },
   on_attach = function (client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client, bufnr)
